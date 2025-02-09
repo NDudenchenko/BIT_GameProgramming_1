2fe617141e7ee71cf3df90c2d9ac53b5d81a4b18
NDudenchenko <n.dudenchenko@gmail.com>
2025-02-09 22:39:51 +0200

    - Created script for spawning sphere projectiles at reaching waypoints

---
+++ Assets/FPS/Scripts/StudyScripts/ShootingObject.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using Unity.FPS.Game;
+using UnityEngine;
+
+namespace AG3962
+{
+    public class ShootingObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] particles;
+        [SerializeField] private Transform objectTransform;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            foreach (var particle in particles)
+            {
+                GameObject someParticle = Instantiate(particle, objectTransform.transform.position + (Vector3.up * 2.0f), Quaternion.identity);
+                StartCoroutine(DestroyingParticles(someParticle));
+            }
+        }
+
+        private IEnumerator DestroyingParticles(GameObject particle)
+        {
+            yield return new WaitForSeconds(5f);
+
+            if (particle)
+            {
+                Destroy(particle);
+            }
+        }
+    }
+}
+
---
+++ Assets/FPS/Scripts/StudyScripts/ShootingTarget.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace AG3962
+{
+    public class ShootingTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform[] targetEndPoints;
+        public Transform target;
+
+        public float speed = 1.0f;
+        private int currentEndPointIndex = 0;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MoveTarget();
+        }
+
+        void MoveTarget()
+        {
+            var step = speed * Time.deltaTime;
+            target.position = Vector3.MoveTowards(target.position, targetEndPoints[currentEndPointIndex].position, step);
+
+            if (Vector3.Distance(target.position, targetEndPoints[currentEndPointIndex].position) < 0.001f)
+            {
+                SwapEndPointTarget();
+            }
+        }
+        void SwapEndPointTarget()
+        {
+            currentEndPointIndex++;
+            currentEndPointIndex %= targetEndPoints.Length;
+        }
+    }
+}
+
+
